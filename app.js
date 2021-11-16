b811d9db0a712506f7f8a084000206c7d83f9028
Kyeom1997 <78855917+Kyeom1997@users.noreply.github.com>
2021-11-16 22:40:53 +0900

    input values

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-const title = document.getElementById("title");
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
 
-title.innerText = "Got you!";
+function onLoginBtnClick() {
+  console.log(loginInput.value);
+}
 
-console.dir(title);
+loginButton.addEventListener("click", onLoginBtnClick);
