f84210e8b3f3eabd6928311ede31907910b51777
Kyeom1997 <78855917+Kyeom1997@users.noreply.github.com>
2021-11-17 22:51:37 +0900

    event

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 
+const link = document.querySelector("a");
+
 function onLoginSubmit(e) {
   e.preventDefault();
   const username = loginInput.value;
   console.log(username);
 }
 
+function handleLinkClick(e) {
+  alert("clicked!");
+  console.log(e);
+}
+
 loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
