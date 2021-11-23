30d78f86db1fc97b10117a7a3f7b1636645d1a93
Kyeom1997 <78855917+Kyeom1997@users.noreply.github.com>
2021-11-23 21:48:41 +0900

    Update todo.js

--- js/todo.js
+++ js/todo.js
@@ -2,10 +2,19 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
-function handleToDoSubmit(event) {
-  event.preventDefault();
-  const newTodo = todoInput.value;
+function handleToDoSubmit(e) {
+  e.preventDefault();
+  const newTodo = toDoInput.value;
   toDoInput.value = "";
+  paintToDo(newTodo);
+}
+
+function paintToDo(newTodo) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  li.appendChild(span);
+  span.innerText = newTodo;
+  toDoList.appendChild(li);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
